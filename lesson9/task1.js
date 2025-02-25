69654ce04c8b908d7f6303c9a6f358e20d98759c
IrynaVakula <vakulaiv@gmail.com>
2025-02-26 00:01:22 +0100

    added homework 9

---
+++ lesson9/task1.js
@@ -0,0 +1,7 @@
+const student = {};
+student.firstName = 'Ivan';
+student.lastName = 'Beck';
+student.age = 30;
+student.courses = ['Math', 'History', 'Programming'];
+
+console.log(student);
---
+++ lesson9/task2.js
@@ -0,0 +1,10 @@
+const book = {
+    title: 'Jungle',
+    author: 'Kipling',
+    year: 1894
+}
+
+const { title, author } = book;
+
+console.log(title);
+console.log(author);
