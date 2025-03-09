4fce58fc45c374841d369caab34c375703632f7a
IrynaVakula <vakulaiv@gmail.com>
2025-03-09 22:33:45 +0100

    fixed errors

--- lesson12/src/task1.js
+++ lesson12/src/task1.js
@@ -1,13 +1,13 @@
 console.log('Start..');
 
 function delayedPrint(text, mls) {
-setTimeout(
-function () {
-            console.log(text)
+    setTimeout(
+        function () {
+            console.log(text);
         },
         mls,
         text,
-    )
+    );
 }
 
 delayedPrint('Task1', 2000);
