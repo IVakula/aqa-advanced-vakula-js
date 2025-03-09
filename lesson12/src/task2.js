2e6a15ccaf75ff0ebc0618a4543505311a87a264
IrynaVakula <vakulaiv@gmail.com>
2025-03-09 22:14:22 +0100

    fixed error

--- lesson12/src/task2.js
+++ lesson12/src/task2.js
@@ -5,13 +5,13 @@ const promise1 = fetch('https://jsonplaceholder.typicode.com/todos/1')
         return Promise.resolve(todo);
     })
     .catch((error) => {
-console.error('Error:', error)
+        console.error('Error:', error);
     });
 
 //subtask 2.2
 const promise2 = fetch('https://jsonplaceholder.typicode.com/users/1')
     .then((response) => response.json())
-.then((user) => {
+    .then((user) => {
         return Promise.resolve(user);
     })
     .catch((error) => {
